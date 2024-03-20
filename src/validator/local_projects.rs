c2e13d301cd0f2a29196d6a0e73088ff0235ea1b
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2024-03-21 00:03:34 +0100

    Fetching release from real data source

--- src/validator/local_projects.rs
+++ src/validator/local_projects.rs
@@ -9,3 +9,30 @@ pub fn locate(path: &str) -> Result<Vec<LocalGradleProject>> {
     let fake = LocalGradleProject::new(gradle_version, DistributionType::Stable, wrapper_checksum, path);
     Ok(vec![fake])
 }
+
+#[cfg(test)]
+pub mod fakes {
+    use crate::validator::models::{DistributionType, LocalGradleProject, Result};
+
+    pub fn locate_tampered_project(path_name: &str) -> Result<Vec<LocalGradleProject>> {
+        let fake = LocalGradleProject::new(
+            "8.5",
+            DistributionType::Stable,
+            "84900f11f4a86050a8f83342ade7b6bc9b0d2bdd-tampered",
+            path_name,
+        );
+
+        Ok(vec![fake])
+    }
+
+    pub fn locate_valid_project(path_name: &str) -> Result<Vec<LocalGradleProject>> {
+        let fake = LocalGradleProject::new(
+            "8.5",
+            DistributionType::Stable,
+            "d3b261c2820e9e3d8d639ed084900f11f4a86050a8f83342ade7b6bc9b0d2bdd",
+            path_name,
+        );
+
+        Ok(vec![fake])
+    }
+}
