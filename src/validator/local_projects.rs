3da83dd3c7e150a211375ec43d7e8a05180370c5
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2024-03-23 23:59:42 +0100

    Retrieving Gradle wrappers from a given file system path (#16)
    * Ditching abstraction around distro types
    
    * Locating real Gradle wrappers according some file path
    
    * Removes useless file
    
    * Enforcing sorted results

--- src/validator/local_projects.rs
+++ src/validator/local_projects.rs
@@ -1,38 +1,77 @@
 // Copyright 2024 Dotanuki Labs
 // SPDX-License-Identifier: MIT
 
-use crate::validator::models::{DistributionType, LocalGradleProject, Result};
+use crate::validator::models::{LocalGradleWrapper, Result};
+use anyhow::{anyhow, ensure};
+use std::path::Path;
+use walkdir::{DirEntry, WalkDir};
 
-pub fn locate(path: &str) -> Result<Vec<LocalGradleProject>> {
-    let wrapper_checksum = "d3b261c2820e9e3d8d639ed084900f11f4a86050a8f83342ade7b6bc9b0d2bdd";
-    let gradle_version = "8.5";
-    let fake = LocalGradleProject::new(gradle_version, DistributionType::Stable, wrapper_checksum, path);
-    Ok(vec![fake])
+pub fn locate(raw_path: &str) -> Result<Vec<LocalGradleWrapper>> {
+    let mut found_wrappers: Vec<LocalGradleWrapper> = WalkDir::new(raw_path)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .map(parse_wrapper_info)
+        .filter_map(|parsed| parsed.ok())
+        .collect();
+
+    ensure!(!&found_wrappers.is_empty(), "No wrappers found");
+
+    found_wrappers.sort_by(|some, another| some.file_system_path.cmp(&another.file_system_path));
+    Ok(found_wrappers)
+}
+
+fn parse_wrapper_info(entry: DirEntry) -> Result<LocalGradleWrapper> {
+    let raw_path = entry.path().to_string_lossy();
+    let complete_path = format!("{}/gradle/wrapper/gradle-wrapper.jar", &raw_path);
+    let wrapper_jar_path = Path::new(&complete_path);
+    let wrapper_checksum = sha256::try_digest(wrapper_jar_path).map_err(|_| anyhow!(""))?;
+    let wrapper = LocalGradleWrapper::new(complete_path.as_ref(), &wrapper_checksum);
+
+    Ok(wrapper)
 }
 
 #[cfg(test)]
 pub mod fakes {
-    use crate::validator::models::{DistributionType, LocalGradleProject, Result};
+    use crate::validator::models::{LocalGradleWrapper, Result};
 
-    pub fn locate_tampered_project(path_name: &str) -> Result<Vec<LocalGradleProject>> {
-        let fake = LocalGradleProject::new(
-            "8.5",
-            DistributionType::Stable,
-            "84900f11f4a86050a8f83342ade7b6bc9b0d2bdd-tampered",
-            path_name,
-        );
-
+    pub fn locate_tampered_project(path_name: &str) -> Result<Vec<LocalGradleWrapper>> {
+        let fake = LocalGradleWrapper::new(path_name, "84900f11f4a86050a8f83342ade7b6bc9b0d2bdd-tampered");
         Ok(vec![fake])
     }
+}
 
-    pub fn locate_valid_project(path_name: &str) -> Result<Vec<LocalGradleProject>> {
-        let fake = LocalGradleProject::new(
-            "8.5",
-            DistributionType::Stable,
-            "d3b261c2820e9e3d8d639ed084900f11f4a86050a8f83342ade7b6bc9b0d2bdd",
-            path_name,
-        );
+#[cfg(test)]
+mod tests {
+    use crate::validator::local_projects::locate;
+    use crate::validator::models::LocalGradleWrapper;
 
-        Ok(vec![fake])
+    #[test]
+    fn should_locate_gradle_wrappers() {
+        let project_dir = std::env::current_dir().unwrap();
+        let test_data_dir = format!("{}/test_data", &project_dir.to_string_lossy());
+
+        let found_wrappers = locate(&test_data_dir).unwrap();
+
+        let gradle7_wrapper_path = format!("{}/gradle7/gradle/wrapper/gradle-wrapper.jar", &test_data_dir);
+        let gradle7_wrapper_sha256 = "575098db54a998ff1c6770b352c3b16766c09848bee7555dab09afc34e8cf590";
+        let gradle7wrapper = LocalGradleWrapper::new(&gradle7_wrapper_path, gradle7_wrapper_sha256);
+
+        let gradle8_wrapper_path = format!("{}/gradle8/gradle/wrapper/gradle-wrapper.jar", &test_data_dir);
+        let gradle8_wrapper_sha256 = "0336f591bc0ec9aa0c9988929b93ecc916b3c1d52aed202c7381db144aa0ef15";
+        let gradle8wrapper = LocalGradleWrapper::new(&gradle8_wrapper_path, gradle8_wrapper_sha256);
+
+        let expected = vec![gradle7wrapper, gradle8wrapper];
+
+        assert_eq!(found_wrappers, expected)
+    }
+
+    #[test]
+    fn should_handle_no_wrappers_found() {
+        let project_dir = std::env::current_dir().unwrap();
+        let scripts_dir = format!("{}/scripts", &project_dir.to_string_lossy());
+
+        let found_wrappers = locate(&scripts_dir);
+
+        assert!(found_wrappers.is_err());
     }
 }
--- src/validator/models.rs
+++ src/validator/models.rs
@@ -5,33 +5,15 @@ use serde::Deserialize;
 
 pub type Result<T> = anyhow::Result<T>;
 
-#[allow(dead_code)]
 #[derive(Debug, Clone, PartialEq)]
-pub enum DistributionType {
-    Stable,
-    Rc,
-    Beta,
-    Milestone,
+pub struct LocalGradleWrapper {
+    pub wrapper_checksum: String,
+    pub file_system_path: String,
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct LocalGradleProject {
-    gradle_version: String,
-    distribution_type: DistributionType,
-    wrapper_checksum: String,
-    file_system_path: String,
-}
-
-impl LocalGradleProject {
-    pub fn new(
-        gradle_version: &str,
-        distribution_type: DistributionType,
-        wrapper_checksum: &str,
-        file_system_path: &str,
-    ) -> Self {
+impl LocalGradleWrapper {
+    pub fn new(file_system_path: &str, wrapper_checksum: &str) -> Self {
         Self {
-            gradle_version: String::from(gradle_version),
-            distribution_type,
             wrapper_checksum: String::from(wrapper_checksum),
             file_system_path: String::from(file_system_path),
         }
@@ -39,19 +21,7 @@ impl LocalGradleProject {
 }
 
 #[derive(Debug, Clone, PartialEq, Deserialize)]
-pub struct GradleRelease {
-    #[serde(rename(deserialize = "version"))]
-    gradle_version: String,
+pub struct OfficialWrapperChecksum {
     #[serde(rename(deserialize = "checksum"))]
-    wrapper_checksum: String,
-}
-
-impl From<&LocalGradleProject> for GradleRelease {
-    fn from(project: &LocalGradleProject) -> Self {
-        let cloned = project.clone();
-        GradleRelease {
-            gradle_version: cloned.gradle_version,
-            wrapper_checksum: cloned.wrapper_checksum,
-        }
-    }
+    pub value: String,
 }
