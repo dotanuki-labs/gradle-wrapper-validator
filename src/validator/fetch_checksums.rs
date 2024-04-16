61823f442dcdabaa535b2bfdc281498cc685056f
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2024-04-17 00:55:01 +0200

    Points to new URL with cached checksums

--- src/validator/fetch_checksums.rs
+++ src/validator/fetch_checksums.rs
@@ -5,8 +5,8 @@ use crate::validator::models::OfficialWrapperChecksum;
 use anyhow::anyhow;
 
 static HOST: &str = "https://cdn.statically.io/gh";
-static GITHUB_REPO: &str = "gradle/wrapper-validation-action";
-static CHECKSUMS_COLLECTION: &str = "main/src/wrapper-checksums.json";
+static GITHUB_REPO: &str = "gradle/actions";
+static CHECKSUMS_COLLECTION: &str = "main/sources/src/wrapper-validation/wrapper-checksums.json";
 
 pub fn fetch() -> anyhow::Result<Vec<OfficialWrapperChecksum>> {
     let releases_url = format!("{}/{}/{}", HOST, GITHUB_REPO, CHECKSUMS_COLLECTION);
