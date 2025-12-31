5c5218fccc6e3c208e2fb78b7c6fe5223a0e6e3b
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2025-12-31 19:46:22 +0100

    feat(validator): use async implementation (#579)
    * feat(validator): use async implementation
    
    * feat(validator): bump deps

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 // Copyright 2025 Dotanuki Labs
 // SPDX-License-Identifier: MIT
 
-use crate::cli::ParsedRawPath;
 use crate::validator::ValidationOutcome;
 use std::process::exit;
 
@@ -11,13 +10,10 @@ mod validator;
 pub static EXIT_CODE_CANNOT_VALID_WRAPPERS: i32 = 13;
 pub static EXIT_CODE_TAMPERED_WRAPPER_FOUND: i32 = 19;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let cli = cli::CommandLineInterface::new();
-    validate_wrappers(cli.parse_arguments());
-}
-
-fn validate_wrappers(target_path: ParsedRawPath) {
-    match validator::locate_and_validate(&target_path.0) {
+    match validator::validate(cli.parse_arguments().0.as_str()).await {
         Ok(outcomes) => ensure_no_issues(outcomes),
         Err(wrapped) => {
             eprintln!("{}", &wrapped.to_string());
--- src/validator.rs
+++ src/validator.rs
@@ -6,10 +6,6 @@ mod find_wrappers;
 
 use serde::Deserialize;
 
-pub fn locate_and_validate(path_name: &str) -> anyhow::Result<Vec<ValidationOutcome>> {
-    validate(path_name, find_wrappers::find, fetch_checksums::fetch)
-}
-
 type ChecksumCheck<'check> = (LocalGradleWrapper, Option<&'check OfficialWrapperChecksum>);
 
 #[derive(Debug, Clone, PartialEq)]
@@ -56,13 +52,9 @@ impl From<ChecksumCheck<'_>> for ValidationOutcome {
     }
 }
 
-fn validate(
-    base_path: &str,
-    locate_gradle_projects: fn(&str) -> anyhow::Result<Vec<LocalGradleWrapper>>,
-    fetch_gradle_releases: fn() -> anyhow::Result<Vec<OfficialWrapperChecksum>>,
-) -> anyhow::Result<Vec<ValidationOutcome>> {
-    let available_checksums = fetch_gradle_releases()?;
-    let local_projects = locate_gradle_projects(base_path)?;
+pub async fn validate(base_path: &str) -> anyhow::Result<Vec<ValidationOutcome>> {
+    let available_checksums = fetch_checksums::fetch().await?;
+    let local_projects = find_wrappers::find(base_path)?;
 
     let validations = local_projects
         .into_iter()
@@ -75,27 +67,27 @@ fn validate(
 
 #[cfg(test)]
 mod tests {
-    use crate::validator::{fetch_checksums, find_wrappers, validate};
+    use crate::validator::validate;
 
     fn project_dir() -> String {
         let root_dir = std::env::current_dir().unwrap();
         String::from(root_dir.to_str().unwrap())
     }
 
-    #[test]
-    fn should_validate_local_project_when_checksum_matches() {
+    #[tokio::test]
+    async fn should_validate_local_project_when_checksum_matches() {
         let valid_wrapper = format!("{}/test-data/valid/gradle8", project_dir());
 
-        let validations = validate(&valid_wrapper, find_wrappers::find, fetch_checksums::fetch).unwrap();
+        let validations = validate(&valid_wrapper).await.unwrap();
         let actual = validations.first().unwrap();
         assert!(actual.has_valid_wrapper_checksum)
     }
 
-    #[test]
-    fn should_validate_local_project_when_checksum_does_not_match() {
+    #[tokio::test]
+    async fn should_validate_local_project_when_checksum_does_not_match() {
         let valid_wrapper = format!("{}/test-data/invalid/tampered", project_dir());
 
-        let validations = validate(&valid_wrapper, find_wrappers::find, fetch_checksums::fetch).unwrap();
+        let validations = validate(&valid_wrapper).await.unwrap();
         let actual = validations.first().unwrap();
         assert!(!actual.has_valid_wrapper_checksum)
     }
