84388143a8f26818940a07a3b6c82b89ab3debea
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2025-12-31 19:31:55 +0100

    feat(validator): use async implementation

--- src/validator/fetch_checksums.rs
+++ src/validator/fetch_checksums.rs
@@ -2,20 +2,55 @@
 // SPDX-License-Identifier: MIT
 
 use crate::validator::OfficialWrapperChecksum;
-use anyhow::anyhow;
+use anyhow::Context;
+use reqwest::header;
+use reqwest_middleware::ClientWithMiddleware;
+use reqwest_retry::RetryTransientMiddleware;
+use reqwest_retry::policies::ExponentialBackoff;
+use std::time::Duration;
 
 static HOST: &str = "https://cdn.statically.io/gh";
 static GITHUB_REPO: &str = "gradle/actions";
 static CHECKSUMS_COLLECTION: &str = "main/sources/src/wrapper-validation/wrapper-checksums.json";
 
-pub fn fetch() -> anyhow::Result<Vec<OfficialWrapperChecksum>> {
+static MAX_HTTP_RETRY_ATTEMPTS: u32 = 2;
+
+pub async fn fetch() -> anyhow::Result<Vec<OfficialWrapperChecksum>> {
+    let http_client = create_http_client()?;
     let releases_url = format!("{HOST}/{GITHUB_REPO}/{CHECKSUMS_COLLECTION}");
 
-    let raw_response = reqwest::blocking::get(&releases_url).expect("Cannot fetch checksums");
-
-    let checksums = raw_response
+    let checksums = http_client
+        .get(&releases_url)
+        .send()
+        .await
+        .context("cannot fetch releases")?
+        .error_for_status()?
         .json()
-        .map_err(|_| anyhow!("Cannot parse Gradle checksums"))?;
+        .await
+        .context("failed to parse gradle releases")?;
 
     Ok(checksums)
+}
+
+fn create_http_client() -> anyhow::Result<ClientWithMiddleware> {
+    let user_agent = format!("{}/{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+
+    let mut headers = header::HeaderMap::new();
+    let user_agent = header::HeaderValue::from_str(&user_agent).expect("invalid header value");
+    let accept = header::HeaderValue::from_str("application/json").expect("invalid header value");
+    headers.insert(header::USER_AGENT, user_agent);
+    headers.insert(header::ACCEPT, accept);
+
+    let base_http_client = reqwest::Client::builder()
+        .default_headers(headers)
+        .timeout(Duration::from_secs(60))
+        .build()
+        .expect("cannot build HTTP client");
+
+    let retry_policy = ExponentialBackoff::builder().build_with_max_retries(MAX_HTTP_RETRY_ATTEMPTS);
+
+    let retrier_http_client = reqwest_middleware::ClientBuilder::new(base_http_client)
+        .with(RetryTransientMiddleware::new_with_policy(retry_policy))
+        .build();
+    Ok(retrier_http_client)
 }
